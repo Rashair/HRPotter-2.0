fd58eeeea894cfec04b21b95c265b60d8b8451e4
Przemysław Wiczołek <przemyslaw.wk@gmail.com>
2019-10-07 13:02:03 +0200

    Initial project

---
+++ HR Potter/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HR_Potter.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
