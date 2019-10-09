63cb596b34af94d99f5a92c9507860a6566bbda4
Przemysław Wiczołek <przemyslaw.wk@gmail.com>
2019-10-09 22:19:27 +0200

    Added basic layout for application form page. Added job application model. Added new view to controller. Added link in layout.

--- HRPotter/Controllers/HomeController.cs
+++ HRPotter/Controllers/HomeController.cs
@@ -28,6 +28,11 @@ namespace HRPotter.Controllers
             return View();
         }
 
+        public IActionResult JobApplication()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
