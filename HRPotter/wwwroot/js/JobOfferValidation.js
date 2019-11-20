d0dc61938fbd4aad1baafa16fd09269baaa9e1de
Przemysław Wiczołek <przemyslaw.wk@gmail.com>
2019-11-20 20:06:57 +0100

    Added validation for job application. Improved validation for job offer.

--- HRPotter/wwwroot/js/JobOfferValidation.js
+++ HRPotter/wwwroot/js/JobOfferValidation.js
@@ -1,30 +1,51 @@
 ﻿$('#JobOfferForm').submit(function (e) {
     let validationResult = true;
 
+    if (!validateSalary()) {
+        validationResult = false;
+        $('#SalaryFrom').focusout(validateSalary);
+        $('#SalaryTo').focusout(validateSalary);
+    }
+    
+
+    if (!validateExpirationDate()) {
+        validationResult = false;
+        $('#ValidUntil').focusout(validateExpirationDate);
+    }
+
+    return validationResult;
+});
+
+function validateSalary() {
+    let result = true;
     let salaryFrom = parseInt($('#SalaryFrom').val());
     let salaryTo = parseInt($('#SalaryTo').val());
     if (!isNaN(salaryFrom) && !isNaN(salaryTo) && salaryFrom > salaryTo) {
         $('#SalaryRange-Error').text("Lower bound of salary cannot be greater than upper bound.");
-        validationResult = false;
+        result = false;
     }
     else {
         $('#SalaryRange-Error').text("");
     }
-    
 
+
+    return result;
+}
+
+function validateExpirationDate() {
+    let result = true;
     let validUntil = new Date($('#ValidUntil').val());
     let currentDate = new Date();
     currentDate.setHours(0, 0, 0, 0);
     if (!isNaN(validUntil) && validUntil < currentDate) {
         $('#PastDate-Error').text("Offer expiration date cannot be past date.");
-        validationResult = false;
+        result = false;
     }
     else {
         $('#PastDate-Error').text("");
     }
-    
 
-    return validationResult;
-});
+    return result;
+}
 
 
