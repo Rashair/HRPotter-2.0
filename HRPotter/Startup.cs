df2364efda56deb143adfd1139aad770346a3b55
Przemysław Wiczołek <przemyslaw.wk@gmail.com>
2019-12-01 00:53:34 +0100

    Some  attemps to make it work - failed

--- HRPotter/Startup.cs
+++ HRPotter/Startup.cs
@@ -65,7 +65,7 @@ namespace HRPotter
             app.UseSwagger();
             app.UseSwaggerUI(c =>
             {
-                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+                c.SwaggerEndpoint("../swagger/v1/swagger.json", "My API V1");
             });
 
 
