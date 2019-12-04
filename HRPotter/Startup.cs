0d05b367db941ca1857ddf11224955b41a51a3b0
Przemysław Wiczołek <przemyslaw.wk@gmail.com>
2019-12-04 09:15:47 +0100

    Add authentication service

--- HRPotter/Startup.cs
+++ HRPotter/Startup.cs
@@ -3,6 +3,8 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Razor;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.AzureADB2C.UI;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -25,7 +27,10 @@ namespace HRPotter
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllersWithViews();
+            services.AddAuthentication(AzureADB2CDefaults.AuthenticationScheme)
+                .AddAzureADB2C(options => Configuration.Bind("AzureAdB2C", options));
+
+            
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "HRPotter API", Version = "v1" });
@@ -35,6 +40,8 @@ namespace HRPotter
                 c.IncludeXmlComments(xmlPath);
             });
 
+            services.AddControllersWithViews();
+            services.AddRazorPages();
 
             services.Configure<RazorViewEngineOptions>(opt =>
             {
@@ -59,6 +66,7 @@ namespace HRPotter
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
@@ -71,6 +79,7 @@ namespace HRPotter
 
             app.UseRouting();
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
@@ -78,6 +87,7 @@ namespace HRPotter
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}");
+                endpoints.MapRazorPages();
             });
         }
     }
