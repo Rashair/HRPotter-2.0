2d36002e87e815baf8d90f8cab33403a4bc506fa
Przemysław Wiczołek <przemyslaw.wk@gmail.com>
2020-05-25 02:20:31 +0200

    Fix buildspec path

--- HRPotter/Helpers/AwsTools.cs
+++ HRPotter/Helpers/AwsTools.cs
@@ -30,37 +30,37 @@ namespace HRPotter.Helpers
             {
                 response = client.GetSecretValueAsync(request).Result;
             }
-            catch (DecryptionFailureException e)
+            catch (DecryptionFailureException)
             {
                 // Secrets Manager can't decrypt the protected secret text using the provided KMS key.
                 // Deal with the exception here, and/or rethrow at your discretion.
                 throw;
             }
-            catch (InternalServiceErrorException e)
+            catch (InternalServiceErrorException)
             {
                 // An error occurred on the server side.
                 // Deal with the exception here, and/or rethrow at your discretion.
                 throw;
             }
-            catch (InvalidParameterException e)
+            catch (InvalidParameterException)
             {
                 // You provided an invalid value for a parameter.
                 // Deal with the exception here, and/or rethrow at your discretion
                 throw;
             }
-            catch (InvalidRequestException e)
+            catch (InvalidRequestException)
             {
                 // You provided a parameter value that is not valid for the current state of the resource.
                 // Deal with the exception here, and/or rethrow at your discretion.
                 throw;
             }
-            catch (ResourceNotFoundException e)
+            catch (ResourceNotFoundException)
             {
                 // We can't find the resource that you asked for.
                 // Deal with the exception here, and/or rethrow at your discretion.
                 throw;
             }
-            catch (System.AggregateException ae)
+            catch (System.AggregateException)
             {
                 // More than one of the above exceptions were triggered.
                 // Deal with the exception here, and/or rethrow at your discretion.
