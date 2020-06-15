4a67c81f942dcac12f1f7c14c6f653147c15ed66
Przemysław Wiczołek <przemyslaw.wk@gmail.com>
2020-06-15 13:07:53 +0200

    Test dynamic creation of db credentials

--- HRPotter/Helpers/Secrets/AWSConnection.cs
+++ HRPotter/Helpers/Secrets/AWSConnection.cs
@@ -2,19 +2,20 @@
 {
     public class AWSConnection : AwsSecret
     {
-        public string Server { get; set; }
-
-        public int Port { get; set; }
-
-        public string Database { get; set; }
-
         public string Username { get; set; }
 
         public string Password { get; set; }
 
+        public string Host { get; set; }
+
+        public int Port { get; set; }
+
+        public string Dbname { get; set; }
+
+
         public override string Value
         {
-            get => $"Server={Server};Port={Port};Database={Database};Uid={Username};Pwd={Password};";
+            get => $"Server={Host};Port={Port};Database={Dbname};Uid={Username};Pwd={Password};";
             set
             {
             }
